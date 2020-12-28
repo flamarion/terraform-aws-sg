5b5d57e897e16c628f5881ef9486c1b7e82812b6
Flamarion Jorge <flamarion@hashicorp.com>
2020-12-28 10:16:10 +0100

    Removing Terraform version constraing from module

--- examples/full-sg.tf
+++ examples/full-sg.tf
@@ -1,7 +1,17 @@
-provider "aws" {
-  version = "~> 2.59"
-  region  = "eu-central-1"
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
 
 
 module "sg" {
